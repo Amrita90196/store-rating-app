e3f5d7cf1ff98144d9281abf31f1cdded325e767
Amrita Singh <amritasingh90196@gmail.com>
2026-05-24 14:58:28 +0530

    Connected PostgreSQL database and created stores table

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,41 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
+const pool = require("./db");
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-const stores = [
-  { name: "Pizza Palace", desc: "Best pizza 🍕", rating: 4.5 },
-  { name: "Burger Hub", desc: "Juicy burgers 🍔", rating: 4.2 },
-  { name: "Cafe Coffee", desc: "Chill vibes ☕", rating: 4.7 }
-];
-
-app.get("/stores", (req, res) => {
-  res.json(stores);
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
+});
+app.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    const bcrypt = require("bcrypt");
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await pool.query(
+      "INSERT INTO users (name, email, password, role) VALUES ($1, $2, $3, $4) RETURNING *",
+      [name, email, hashedPassword, role]
+    );
+
+    res.json(newUser.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
 });
